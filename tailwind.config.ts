2d84b5f5c10dd69012655c9981a2dc58185c4329
jpedro002 <joaopsilvavolei@gmail.com>
2024-03-14 10:50:59 -0300

    feat: colors of typography on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundColor: {
+        'dark-gray': '#1F1F1F',
+        'dark-gray-2': '#474747',
+        'dark-gray-3': '#666',
+        'light-gray': '#8F8F8F',
+        'light-gray-2': '#CCC',
+        'light-gray-3': '#F5F5F5',
+        primary: '#C92071',
+        secondary: '#B5B6F2',
+        terciary: '#991956',
+        error: 'EE4266',
+        success: '#52CA76',
+        warning: '#F6AA1C',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config

2d31e7e19402134ef2607b12a64e0e4fa74cc7e4
Mateus Bessa <mateusbessacontato@gmail.com>
2024-03-15 16:34:07 -0300

    Links and sections implemented on footer

--- src/components/FooterStore/FooterStore.tsx
+++ src/components/FooterStore/FooterStore.tsx
@@ -1,13 +1,14 @@
-import { footerIcons } from '@/constants'
+import { footerIcons, footerLinks } from '@/constants'
 import Image from 'next/image'
 
 
 export const FooterStore = () => {
     return (
         <footer className="max-container">
-            <div className="flex flex-wrap justify-between items-start gap-20 max-lg:flex-col">
+            <div className="flex flex-wrap justify-between items-start lg:gap-44 gap-20 max-lg:flex-col">
                 <div className="flex flex-col items-start">
-                    <a href="">
+                    <a className='flex gap-4' href="">
+                        <Image src="/group.svg" alt="Digital Store" width={32} height={32} />
                         <h2 className="text-4xl font-bold text-white">Digital Store</h2>
                     </a>
                     <p className='text-white mt-8 leading-7 sm:max-w-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, earum in! Possimus culpa, ad alias a eveniet nam aliquid!</p>
@@ -18,6 +19,21 @@ export const FooterStore = () => {
                             </div>
                         ))}
                     </div>
+                </div>
+
+                <div className='flex flex-1 justify-between flex-wrap lg:gap-10 gap-20'>
+                    {footerLinks.map((section) => (
+                        <div>
+                            <h4 className='text-white text-xl leading-normal font-medium mb-6'>{section.title}</h4>
+                            <ul>
+                                {section.links.map((link) => (
+                                    <li className='mt-3 text-white text-base leading-normal hover:text-slate-500 cursor-pointer'>
+                                        <a>{link.name}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
             </div>
         </footer>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,36 @@
 export const footerIcons = [
-    {src: 'F', alt: 'test'},
-    {src: 'I', alt: 'test'},
-    {src: 'T', alt: 'test'},
+    { src: 'F', alt: 'test' },
+    { src: 'I', alt: 'test' },
+    { src: 'T', alt: 'test' },
+]
+
+export const footerLinks = [
+    {
+        title: 'Informação',
+        links: [
+            { name: 'Sobre Drip Store', link: '' },
+            { name: 'Segurança', link: '' },
+            { name: 'Wishlist', link: '' },
+            { name: 'Blog', link: '' },
+            { name: 'Trabalhe conosco', link: '' },
+            { name: 'Meus pedidos', link: '' },
+        ]
+    },
+    {
+        title: 'Categorias',
+        links: [
+            { name: 'Camisetas', link: '' },
+            { name: 'Calças', link: '' },
+            { name: 'Bonés', link: '' },
+            { name: 'Headphones', link: '' },
+            { name: 'Tênis', link: '' },
+        ]
+    },
+    {
+        title: 'Contato',
+        links: [
+            { name: 'Endereço: Av.Santos Dumont, 1510', link: '' },
+            { name: 'Telefone: (85)3051-3411', link: '' },
+        ]
+    }
 ]
